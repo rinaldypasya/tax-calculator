67714c5838d2d1a83df05493a2380e122389a90f
Rinaldy Pasya <rinaldypasya@gmail.com>
2019-05-30 19:45:58 +0700

    Add error helper

---
+++ src/helpers/error_helper.go
@@ -0,0 +1,40 @@
+package helpers
+
+import (
+	"../constants"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
+)
+
+type ErrorHelper struct {
+}
+
+func ErrorHelperHandler() (ErrorHelper) {
+	return ErrorHelper{}
+}
+
+func (handler *ErrorHelper) HTTPResponseError(context *gin.Context, e error, defaultErrorCode int) {
+
+	if nil != e {
+		switch e.Error() {
+		case "record not found":
+			defaultErrorCode = constants.ResourceNotFound
+			break
+		}
+	}
+
+	errorConstant := constants.GetErrorConstant(defaultErrorCode)
+	context.JSON(errorConstant.HttpCode, gin.H{
+		"code":    defaultErrorCode,
+		"message": errorConstant.Message,
+	})
+
+	if nil != e {
+		if _, ok := e.(*mysql.MySQLError); !ok {
+			fmt.Println(e)
+		}
+		panic(e)
+	}
+
+}
